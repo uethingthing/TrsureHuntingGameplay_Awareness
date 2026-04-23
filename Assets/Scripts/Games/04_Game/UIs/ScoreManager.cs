ef7e0e05fff4a193bce3fdf9e32513ca6fdcfcaa
pro_sakae <sakae@prolead.co.jp>
2026-04-23 10:30:43 +0900

    スコア表示の画像差し替え

--- Assets/Scripts/Games/04_Game/UIs/ScoreManager.cs
+++ Assets/Scripts/Games/04_Game/UIs/ScoreManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -17,48 +16,27 @@ public class ScoreManager : MonoBehaviour
     //--------------------------------------------
 
     [SerializeField]
-    private Text m_totalPointText;
+    private TextMeshProUGUI m_totalPointText;
 
     [SerializeField]
-    private Text m_youPointText;
+    private TextMeshProUGUI m_totalOpponentPointText;
 
     [SerializeField]
-    private Text m_addYouPointText;
+    private TextMeshProUGUI m_addPointText;
 
     [SerializeField]
-    private Text m_opponentPointText;
-
-    [SerializeField]
-    private Text m_addOpponentPointText;
-
-    [SerializeField]
-    private Text m_totalOpponentPointText;
-
-    //--------------------------------------------
-    // データ
-    //--------------------------------------------
-
-    private int m_addYouScore;
-
-    private int m_addOpponentScore;
+    private TextMeshProUGUI m_addOpponentPointText;
 
     //--------------------------------------------
     // 初期化
     //--------------------------------------------
-
-    // Start is called before the first frame update
     void Start()
     {
         m_totalPointText.text = "0";
-        m_youPointText.text = "0";
-        m_opponentPointText.text = "0";
         m_totalOpponentPointText.text = "0";
 
-        m_addYouPointText.text = "";
-        m_addOpponentPointText.text = "";
-
-        m_addYouScore = 0;
-        m_addOpponentScore = 0;
+        m_addPointText.text = string.Empty;
+        m_addOpponentPointText.text = string.Empty;
     }
 
     //--------------------------------------------
@@ -74,15 +52,13 @@ public class ScoreManager : MonoBehaviour
             UserData opponentData = GameInfo.OpponentData;
             // スコア表示更新
             m_totalPointText.text = myData.TotalScore.ToString();
-            m_youPointText.text = myData.Score.ToString();
-            m_opponentPointText.text = opponentData.Score.ToString();
             m_totalOpponentPointText.text = opponentData.TotalScore.ToString();
         }
     }
 
     public void AddScore(int score, bool isYouTurn)
     {
-        string strScore;
+        string strScore = string.Empty;
         if (score > 0)
         {
             strScore = "+" + score.ToString();
@@ -94,12 +70,10 @@ public class ScoreManager : MonoBehaviour
 
         if (isYouTurn)
         {
-            m_addYouScore = score;
-            m_addYouPointText.text = strScore;
+            m_addPointText.text = strScore;
         }
         else
         {
-            m_addOpponentScore = score;
             m_addOpponentPointText.text = strScore;
         }
         StartCoroutine(InVisibleAddScore());
@@ -108,7 +82,7 @@ public class ScoreManager : MonoBehaviour
     private IEnumerator InVisibleAddScore()
     {
         yield return new WaitForSeconds(ADD_SCORE_VISIBLE_TIME);
-        m_addYouPointText.text = "";
-        m_addOpponentPointText.text = "";
+        m_addPointText.text = string.Empty;
+        m_addOpponentPointText.text = string.Empty;
     }
 }
